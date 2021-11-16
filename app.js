c01dc890d3eb043d608d9e67e1d992157e9035a6
Alex <aptaylor87@gmail.com>
2021-11-15 16:33:13 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const token = "aQ0F6loLrpzjl1v9jtDNyyLd7I1Cjdcu";
+
+//click listener to grab the search 
+$('#giphy-search').on('submit', async function(evt) {
+    evt.preventDefault();
+    let searchTerm = $('#search-term').val();
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { q: searchTerm, api_key: token } });
+    addGif(res.data);
+    $('#search-term').val("");
+
+});
+
+//request gifs with a certain query term
+
+function addGif(response) {
+    if(response) {
+        let randomIndex = Math.floor(Math.random() * 50);
+        let $newDiv = $('<div>', { class: 'giphy-gif'});
+        let $newGif = $('<img>', { src: response.data[randomIndex].images.original.url, class: "gif-itself" });
+        $newDiv.append($newGif);
+        $('.gif-area').append($newDiv);
+
+        
+    }
+}
+
+$('#delete').on('click', function() {
+    $('#gif-area').empty();
+});
+
